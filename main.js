3cca20b8c7c5ad96781d3998c700fb6151d94f4e
Andriy Hushchyn <hyshchun@gmail.com>
2021-09-14 20:56:42 +0300

    function getSqrt is already done

---
+++ main.js
@@ -0,0 +1,19 @@
+function getSqrt(num) {
+    if (typeof num == 'undefined') {
+        console.log('Будь-ласка введіть число');
+    }
+    else if (typeof num !== 'number') {
+        console.log('Повинно бути числове значення');
+    }
+    else if (num < 0) {
+        console.log('Введіть додатнє число');
+    }
+    else {
+        let result = Math.sqrt(num);
+        console.log(result);
+    }
+}
+getSqrt('wewe');
+getSqrt(9);
+getSqrt(-9);
+getSqrt();
